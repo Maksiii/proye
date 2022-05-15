d88bc7805503dae2083e57d218f33159f09b9c62
Maksiii <102603891+Maksiii@users.noreply.github.com>
2022-05-15 19:13:55 -0400

    Add files via upload
    probando primer proye

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import ModelForm
+from .models import *
+
+#creamos un tetmplate del formulario
+class ProductoForm(ModelForm):
+    
+    #validar que los campos sean limitados
+    # nombre = forms.CharField(min_length=10,max_length=20)
+    # precio = forms.IntegerField(min_value=400)
+
+    class Meta:
+        model = Producto
+        fields = ['plu_codigo','nombre','marca','precio','stock','tipo','imagen']
+
+      
